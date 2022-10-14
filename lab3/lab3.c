0ad8d6bef793aaaaebc72c78663f72c9a30b33a4
Maxwell Freudenburg <34214992+CodeMonkeyMax@users.noreply.github.com>
2022-10-14 18:25:51 +0000

    Implemented Testpoint

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -6,25 +6,17 @@
 #include <stdio.h>
 
 // initialize vars here
-#define MAXOBJS 10
 #define MAXPTS 59000
 #define MAXPOLYS 57500
 
-int numpoints[MAXOBJS];			// NOT poly size!
-double centerpoint[MAXOBJS][2]; // Definitely keep this, dummy
-double x[MAXOBJS][MAXPTS], y[MAXOBJS][MAXPTS];
-int numpolys[MAXOBJS];
-int numobjs;
-int psize[MAXOBJS][MAXPOLYS];
-int con[MAXOBJS][MAXPOLYS][20];
-double poly_rgb[MAXOBJS][MAXPOLYS][3];
+double centerpoint[MAXPOLYS][2]; // Definitely keep this, dummy
+double x[MAXPOLYS][MAXPTS], y[MAXPOLYS][MAXPTS];
+int numpolys;
+int psize[MAXPOLYS];
 
 // TODO: I will need to change the format of these variables slightly,
 // as they are not quite right for this use case. I think I need to
 // get rid of the OBJECTS entirely
-
-// THEN AGAIN, I will probably need to be able to support OBJECTS in
-// order to support convex culling windows
 
 // OBJECTIVE: create a poly.
 // clicks inside will G_draw a circle of one color,
@@ -34,12 +26,13 @@ int occlude_and_redraw() {
 	return 0;
 }
 
-int testpoint(double p[2], int pindex) {
+// Returns 1 if point p is outside a line on polygon poli. Else 0
+int testpoint(double p[2], int poli) {
 	// make lines & test lines? probably don't need to make lines, just test
 	// points in for loop with my signature "end-case if statement" for
 	// cleanliness
 	// double lines[MAXPTS / 2][2][2], ymin, ymax, xmin, xmax;
-	int size = psize[0][pindex];
+	int size = psize[poli];
 	// for each line in poly: check centerpoint
 	int j;
 	for (int i = 0; i < size; i++) {
@@ -47,19 +40,25 @@ int testpoint(double p[2], int pindex) {
 		if (i == size - 1) {
 			j = 0;
 		}
-		double dx = x[0][j] - x[0][i], dy = y[0][j] - y[0][i];
-		// dy * Y - dy * X = dx*y0 - dy * x0
+		double dx = x[poli][j] - x[poli][i], dy = y[poli][j] - y[poli][i];
+		if ((dx * p[1] - dy * p[0] - dx * y[poli][i] - dy * x[poli][i]) *
+				(dx * centerpoint[poli][1] - dy * centerpoint[poli][0] -
+				 dx * y[poli][i] - dy * x[poli][i]) <
+			0) {
+			return 1;
+		}
 	}
+	return 0;
 }
 
-void findcenterpoint(double out[2], int obji) {
+void findcenterpoint(double out[2], int poli) {
 	double s[2] = {0, 0}; // sum
-	for (int i = 0; i < psize[obji][obji]; i++) {
-		s[0] += x[obji][i];
-		s[1] += y[obji][i];
+	for (int i = 0; i < psize[poli]; i++) {
+		s[0] += x[poli][i];
+		s[1] += y[poli][i];
 	}
-	out[0] = s[0] / psize[obji][obji];
-	out[1] = s[1] / psize[obji][obji];
+	out[0] = s[0] / psize[poli];
+	out[1] = s[1] / psize[poli];
 }
 
 int main() {
@@ -75,7 +74,7 @@ int main() {
 	// double centerpoint[2];
 	// draw 1st polygon
 	G_rgb(0.8, 0, 0.4);
-	psize[0][0] = click_and_save(x[0], y[0]);
+	psize[0] = click_and_save(x[0], y[0]);
 	printf("click_and_save 1 success\n");
 	findcenterpoint(centerpoint[0], 0);
 
@@ -88,7 +87,11 @@ int main() {
 		printf("click_and_save success\n");
 		G_wait_click(in);
 
-		// test in
+		if (testpoint(in, 0) == 1) {
+			G_rgb(1, 0, 0);
+		} else {
+			G_rgb(0, 1, 0);
+		}
 
 		G_circle(in[0], in[1], 3);
 	} while (in[1] > done_area_y);
@@ -96,72 +99,4 @@ int main() {
 	// draw 2nd polygon
 	// numpoints[1] = click_snap_and_save(x[1], y[1]);
 	// clip 1st polygon by 2nd polygon & redraw
-}
-
-int xy_poly_fill(double x[], double y[], int size, double rgb[3]) {
-	int ymin = y[0], ymax = y[0];
-
-	G_rgb(rgb[0], rgb[1], rgb[2]);
-
-	// initialize line array
-	double lines[1000][2][2];
-
-	// store lines
-	for (int i = 0; i < size - 1; i++) {
-		if (y[i + 1] < ymin)
-			ymin = y[i + 1];
-		if (y[i + 1] > ymax)
-			ymax = y[i + 1];
-
-		lines[i][0][0] = x[i];
-		lines[i][0][1] = y[i];
-
-		lines[i][1][0] = x[i + 1];
-		lines[i][1][1] = y[i + 1];
-	}
-	lines[size - 1][0][0] = x[size - 1];
-	lines[size - 1][0][1] = y[size - 1];
-	lines[size - 1][1][0] = x[0];
-	lines[size - 1][1][1] = y[0];
-
-	// start scan line loop
-	for (int i = 0; i < screen.y; i++) {
-		// if (i % 10 == 0)
-		//	G_wait_key();
-		double xhit[1000];
-		// index of xhit. Resets on every new scan line
-		int pint = 0;
-
-		for (int j = 0; j < size; j++) {
-			// check_line() called
-			// takes start & end points, a reference to an array of intersection
-			// points, and current scan line y value
-			double test = check_line(lines[j][0], lines[j][1], (double)i);
-			if (test != 0) {
-				xhit[pint++] = test;
-			}
-		}
-
-		if (pint >= 2) {
-			// printf("i: %4i\n", pint);
-			sort(xhit, pint);
-			for (int j = 1; j < pint; j++) {
-				for (int k = 1; k < pint; k++) {
-					double x0 = xhit[k - 1];
-					double x1 = xhit[k];
-					if (x0 > x1) {
-						xhit[k] = x1;
-						xhit[k - 1] = x0;
-					}
-				}
-			}
-
-			for (int j = 0; j < pint; j += 2) {
-				// printf("draw ( %3.0lf, %3.0lf )\n", xhit[j], xhit[j + 1]);
-				G_line(xhit[j], i, xhit[j + 1], i);
-			}
-		}
-	}
-
-	return 0;
 }
